4ffb73a924b5e5568f5e865a6a5f03242d551aa2
Reza Mohammadi <reza.mohammadi9727@gmail.com>
2025-02-08 19:08:51 +0330

    Create Booklist and add styles to project

---
+++ src/components/BookCard.jsx
@@ -0,0 +1,27 @@
+import React, { useState } from 'react';
+import { GiHeartWings  } from "react-icons/gi";
+
+import styles from './BookCard.module.css';
+
+function BookCard({data:{title,author,image,language,pages}}) {
+    const [like , setLike]=useState(false)
+    const likeHandler=()=>{
+        setLike((like)=>!like)
+    }
+  return (
+    <div className={styles.card}>
+        <img src={image} alt={title}/>
+        <div className={styles.info}>
+            <h3>{title}</h3>
+            <p>{author}</p>
+            <div>
+                <span>{language}</span>
+                <span>{pages}pages</span>
+            </div>
+        </div>
+        <button onClick={likeHandler}><GiHeartWings  color={like ? "red" : "#e0e0e0" } fontSize="1.8rem"/></button>
+    </div>
+  )
+}
+
+export default BookCard
